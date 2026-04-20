138bc3f1da663886f42decfef43da0e7be543735
An-an-525 <an-an-525@users.noreply.github.com>
2026-04-20 09:41:25 +0000

    fix: API_BASE改用HTTPS避免Mixed Content拦截

--- js/utils.js
+++ js/utils.js
@@ -6,7 +6,7 @@
 var API_BASE = (function(){
   var h = location.hostname;
   if(h.indexOf('github.io') >= 0 || h.indexOf('localhost') >= 0) {
-    return 'http://149.28.143.114';
+    return 'https://149.28.143.114';
   }
   return '';
 })();
